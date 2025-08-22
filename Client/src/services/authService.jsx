dfb7c56e93e0ffaad3857c81600207f189287470
realarjun2005-eng <realarjun2005@gmail.com>
2025-08-22 23:08:08 +0530

    Update authService.jsx

--- Client/src/services/authService.jsx
+++ Client/src/services/authService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://invest-2-9yoa.onrender.com/api/auth';
 
 export const registerUser = async (data) => {
   const res = await axios.post(`${API_URL}/register`, data);
